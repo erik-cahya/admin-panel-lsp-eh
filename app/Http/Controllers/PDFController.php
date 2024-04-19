4c52900c8741324bf17f877e1696bf460f972797
erik-cahya <erik.cahya841@gmail.com>
2024-04-20 01:51:49 +0800

    test-pdf

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SuratTugasModel;
 use Illuminate\Http\Request;
 use PDF;
 
@@ -12,7 +13,10 @@ class PDFController extends Controller
     public function cetak(){
 
 
-        $pdf = PDF::loadView('cetakpdf');
+
+        $data['dataSurat'] = SuratTugasModel::where('id', 1)->first();
+        // dd($data['dataSurat']);
+        $pdf = PDF::loadView('admin.surat.surat-tugas-asesor.pdf', $data);
         return $pdf->stream('Laporan-Data-Santri.pdf');
     }
 }
