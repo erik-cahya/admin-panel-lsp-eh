38a1550b9265efa610640021f91627819e3912a1
erik-cahya <erik.cahya841@gmail.com>
2024-04-20 01:01:39 +0800

    update: breeze installed

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PDFController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('cetak', [PDFController::class, 'cetak'])->name('cetakPDF');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+require __DIR__.'/auth.php';
